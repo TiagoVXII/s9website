71e3ac8117818295b43d31027b32c435a277dda9
Flas4hPT <93576144+TiagoVXII@users.noreply.github.com>
2023-02-11 09:57:36 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,7 +67,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/Thumbnail.png',
+      image: 'img/favicon.ico',
       navbar: {
         title: 'Sigma-9 Website',
         logo: {
